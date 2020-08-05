5343e5e9119bf78e0d5aaa24b84399270ad9eaac
Daniel R. Fiala <freelancer@meave.biz>
2020-08-05 09:15:18 +0200

    tests/tshark-with-ocaml-node/bin/start-tezos-node.sh: Store jsons with RPC outputs.

--- tests/tshark-with-ocaml-node/bin/start-tezos-node.sh
+++ tests/tshark-with-ocaml-node/bin/start-tezos-node.sh
@@ -12,5 +12,12 @@ cp -vf /usr/local/T3z0s/configs/identity.json /var/run/tezos/node/data
 
 find / -name identity.json
 
-( sleep 180; killall tezos-node ) &
+( sleep 180; ip link set eth0 down ) &
+( sleep 300; killall tezos-node ) &
+(
+    sleep 240;
+    wget -O /root/connections.json 'http://localhost:8732/network/connections';
+    wget -O /root/points.json 'http://localhost:8732/network/points';
+    wget -O /root/peers.json 'http://localhost:8732/network/peers';
+) &
 /usr/local/bin/entrypoint.sh tezos-node --net-addr :19732 --network "$TEZOS_NETWORK"
