61a7d7d179cc3daf955d93d668ef843695cf0919
Daniel R. Fiala <freelancer@meave.biz>
2020-08-06 18:03:17 +0200

    Simple test: Applied rustfmt.

--- tilraunir/cmp-json-and-tshark/src/main.rs
+++ tilraunir/cmp-json-and-tshark/src/main.rs
@@ -1,18 +1,16 @@
 extern crate regex;
 
-use std::net::SocketAddr::V6;
-use std::net::SocketAddr::V4;
-use serde_json::{Value};
+use failure::Error;
+use regex::Regex;
+use serde_json::Value;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddr::V4, SocketAddr::V6, SocketAddrV4};
 use std::option::Option;
 use std::path::Path;
-use regex::Regex;
 use std::result::Result;
 use std::vec::Vec;
-use failure::Error;
 
 #[derive(Debug, Clone)]
 struct ConnectionFromRpc {
@@ -42,7 +40,7 @@ fn unify_addr(addr: SocketAddr) -> SocketAddr {
 }
 
 fn parse_json(file_path: &str) -> Result<Value, Error> {
-    let v: Value = serde_json::from_str( &std::fs::read_to_string(file_path)? )?;
+    let v: Value = serde_json::from_str(&std::fs::read_to_string(file_path)?)?;
     return Ok(v);
 }
 
@@ -50,71 +48,106 @@ fn process_connections(file_path: &str) -> Result<HashMap<String, ConnectionFrom
     let mut m = HashMap::new();
 
     let res = parse_json(file_path);
-    res.map(|r| r.as_array().map(|arr| {
-        for v in arr {
-            let conn_opt = || -> Option<ConnectionFromRpc> {
-                //println!("v:{:?}", v);
-                let peer_id = v.pointer("/peer_id").and_then(|v| v.as_str()).
-                              map(|s| s.to_string())?;
-                //println!("peer_id:{:?}", peer_id);
-                let addr = v.pointer("/id_point/addr").and_then(|v| v.as_str()).
-                              and_then(|s| s.parse::<IpAddr>().ok())?;
-                //println!("addr::{:?}", addr);
-                let port = v.pointer("/id_point/port").and_then(|v| v.as_u64()).
-                              and_then(|u| if (u as u16) as u64 == u { Some(u as u16) } else { None })?;
-                //println!("port:{:?}", port);
-                //let chain_name = v.pointer("/announced_version/chain_name");
-                //println!("\tpeer_id:{:?}; addr:{:?}; port:{:?}; chain_name:{:?}", peer_id, addr, port, chain_name);
-                Some(ConnectionFromRpc{
-                    peer_id,
-                    addr: unify_addr(SocketAddr::new(addr, port)),
-                })
-            }();
-            if let Some(conn) = conn_opt {
-                let peer_id = conn.peer_id.clone();
-                m.insert(peer_id, conn);
+    res.map(|r| {
+        r.as_array().map(|arr| {
+            for v in arr {
+                let conn_opt = || -> Option<ConnectionFromRpc> {
+                    //println!("v:{:?}", v);
+                    let peer_id = v
+                        .pointer("/peer_id")
+                        .and_then(|v| v.as_str())
+                        .map(|s| s.to_string())?;
+                    //println!("peer_id:{:?}", peer_id);
+                    let addr = v
+                        .pointer("/id_point/addr")
+                        .and_then(|v| v.as_str())
+                        .and_then(|s| s.parse::<IpAddr>().ok())?;
+                    //println!("addr::{:?}", addr);
+                    let port = v
+                        .pointer("/id_point/port")
+                        .and_then(|v| v.as_u64())
+                        .and_then(|u| {
+                            if (u as u16) as u64 == u {
+                                Some(u as u16)
+                            } else {
+                                None
+                            }
+                        })?;
+                    //println!("port:{:?}", port);
+                    //let chain_name = v.pointer("/announced_version/chain_name");
+                    //println!("\tpeer_id:{:?}; addr:{:?}; port:{:?}; chain_name:{:?}", peer_id, addr, port, chain_name);
+                    Some(ConnectionFromRpc {
+                        peer_id,
+                        addr: unify_addr(SocketAddr::new(addr, port)),
+                    })
+                }();
+                if let Some(conn) = conn_opt {
+                    let peer_id = conn.peer_id.clone();
+                    m.insert(peer_id, conn);
+                }
             }
-        }
-    }))?;
+        })
+    })?;
 
     Ok(m)
 }
 
-fn process_peers(file_path: &str,
-                 conns_from_rpc: &HashMap<String, ConnectionFromRpc>,
-                 conn_msgs: &Vec<ConnectionMsg>) -> Result<(), Error> {
+fn process_peers(
+    file_path: &str,
+    conns_from_rpc: &HashMap<String, ConnectionFromRpc>,
+    conn_msgs: &Vec<ConnectionMsg>,
+) -> Result<(), Error> {
     let res = parse_json(file_path);
 
     //println!("conn_from_rpc:{:?}", conns_from_rpc);
     //println!("conn_msgs:{:?}", conn_msgs);
 
-    res.map(|r| r.as_array().map(|arr| {
-        for v in arr {
-            //println!("v:{:?}", v);
-            let peer_id = v.pointer("/0").and_then(|v| v.as_str()).unwrap_or("").to_owned();
-            let total_sent = v.pointer("/1/stat/total_sent").and_then(|v| v.as_str()).
-                             and_then(|s| s.parse::<usize>().ok()).unwrap_or(0);
-            let total_received = v.pointer("/1/stat/total_recv").and_then(|v| v.as_str()).
-                             and_then(|s| s.parse::<usize>().ok()).unwrap_or(0);
-            //println!("\tpeer_id:{:?}; total_sent:{:?}; total_received:{:?}", peer_id, total_sent, total_received);
+    res.map(|r| {
+        r.as_array().map(|arr| {
+            for v in arr {
+                //println!("v:{:?}", v);
+                let peer_id = v
+                    .pointer("/0")
+                    .and_then(|v| v.as_str())
+                    .unwrap_or("")
+                    .to_owned();
+                let total_sent = v
+                    .pointer("/1/stat/total_sent")
+                    .and_then(|v| v.as_str())
+                    .and_then(|s| s.parse::<usize>().ok())
+                    .unwrap_or(0);
+                let total_received = v
+                    .pointer("/1/stat/total_recv")
+                    .and_then(|v| v.as_str())
+                    .and_then(|s| s.parse::<usize>().ok())
+                    .unwrap_or(0);
+                //println!("\tpeer_id:{:?}; total_sent:{:?}; total_received:{:?}", peer_id, total_sent, total_received);
 
-            if total_sent > 0 || total_received > 0 {
-                let msg_from_rpc = conns_from_rpc.get(&peer_id).unwrap();
-                let item = conn_msgs.iter().find(|msg| msg.src_addr == msg_from_rpc.addr || msg.dst_addr == msg_from_rpc.addr);
-                if item.is_none() {
-                    eprintln!("Cannot find connection for peer_id:{}, addr:{} :-(", peer_id, msg_from_rpc.addr);
-                } else {
-                    eprintln!("Found connection for peer_id:{} :-)", peer_id);
+                if total_sent > 0 || total_received > 0 {
+                    let msg_from_rpc = conns_from_rpc.get(&peer_id).unwrap();
+                    let item = conn_msgs.iter().find(|msg| {
+                        msg.src_addr == msg_from_rpc.addr || msg.dst_addr == msg_from_rpc.addr
+                    });
+                    if item.is_none() {
+                        eprintln!(
+                            "Cannot find connection for peer_id:{}, addr:{} :-(",
+                            peer_id, msg_from_rpc.addr
+                        );
+                    } else {
+                        eprintln!("Found connection for peer_id:{} :-)", peer_id);
+                    }
                 }
             }
-        }
-    }))?;
+        })
+    })?;
 
     Ok(())
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
@@ -139,8 +172,10 @@ fn parse_tshark(file_path: &str) -> Result<Vec<ConnectionMsg>, Error> {
         let line = line_res?;
         if re_empty_line.is_match(&line) {
             let conversation = conversation_opt.unwrap_or("0x0".to_string());
-            let src_addr = src_addr_opt.unwrap_or(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0));
-            let dst_addr = dst_addr_opt.unwrap_or(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0));
+            let src_addr =
+                src_addr_opt.unwrap_or(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0));
+            let dst_addr =
+                dst_addr_opt.unwrap_or(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0));
             if let Some(msg) = conn_msg_opt {
                 conn_msgs.push(ConnectionMsg {
                     conversation: conversation,
