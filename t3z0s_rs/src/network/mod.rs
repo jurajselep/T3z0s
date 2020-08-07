4a748704699c9b1edd1536bdccd88a3dae23e8e8
Daniel R. Fiala <freelancer@meave.biz>
2020-08-07 12:23:34 +0200

    Refactoring: Remove ynysed parts from network.

--- t3z0s_rs/src/network/mod.rs
+++ t3z0s_rs/src/network/mod.rs
@@ -3,14 +3,9 @@
 
 pub mod raw_packet_msg;
 pub mod msg_decoder;
-// pub mod msg_encoder;
-//pub mod tun_bridge;
 pub mod connection_message;
-
-//mod health_checks;
 
 pub mod prelude {
     pub use super::connection_message::*;
     pub use super::msg_decoder::EncryptedMessageDecoder;
-    //pub use super::tun_bridge::make_bridge;
 }
