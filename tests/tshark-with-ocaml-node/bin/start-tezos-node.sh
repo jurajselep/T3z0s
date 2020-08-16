45dfc5eea23f120ac4080956ec09495b7786c5ba
Daniel R. Fiala <freelancer@meave.biz>
2020-08-16 12:38:47 +0200

    start-tezos-node.sh: Remove unused code.

--- tests/tshark-with-ocaml-node/bin/start-tezos-node.sh
+++ tests/tshark-with-ocaml-node/bin/start-tezos-node.sh
@@ -5,12 +5,9 @@ set -xeo pipefail
 TSHARK_READY=/tmp/tshark.ready
 TEZOS_NETWORK=carthagenet
 
-whoami
 counter=0
 until [ "$counter" -gt 1000 -o -f "$TSHARK_READY" ]; do sleep 1; done
 cp -vf /usr/local/Tezos/configs/identity.json /var/run/tezos/node/data
-
-find / -name identity.json
 
 ( sleep 180; ip link set eth0 down ) &
 ( sleep 300; killall tezos-node ) &
